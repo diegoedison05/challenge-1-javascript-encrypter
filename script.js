3fa9aa4f99c1538301b495101c3c4f9d20700d94
DiegoEdison <diegoedison05@gmail.com>
2022-12-28 15:09:10 -0500

    Corrigiendo la función de encriptar para que tome también las letras con tilde

--- script.js
+++ script.js
@@ -7,6 +7,8 @@
 const textAreaIn = document.querySelector('#textIn');
 const btnEncrypt = document.querySelector('#encry');
 const pResult = document.querySelector('.pResult');
+let btnYellow = document.querySelector('body');
+const searchLogo = document.querySelector('.searchLogo');
 
 btnEncrypt.addEventListener('click', btnOnClick);
 // console.log(textIn);
@@ -18,20 +20,36 @@ function btnOnClick() {
     // textLower = textLower.split('');
     let textencrypt = '';
     for (let i = 0; i < textLower.length; i++) {
-        if (textLower[i] == 'a') {
+        if (textLower[i] == 'a' || textLower[i] == 'á') {
             textencrypt = textencrypt + 'ai';
-        } else if (textLower[i] == 'e') {
+        } else if (textLower[i] == 'e' || textLower[i] == 'é') {
             textencrypt = textencrypt + 'enter';
-        } else if (textLower[i] == 'i') {
+        } else if (textLower[i] == 'i' || textLower[i] == 'í') {
             textencrypt = textencrypt + 'imes';
-        } else if (textLower[i] == 'o') {
+        } else if (textLower[i] == 'o' || textLower[i] == 'ó') {
             textencrypt = textencrypt + 'ober';
-        } else if (textLower[i] == 'u') {
+        } else if (textLower[i] == 'u' || textLower[i] == 'ú') {
             textencrypt = textencrypt + 'ufat';
         } else {
             textencrypt = textencrypt + textLower[i];
         }
     }
     console.log('textencrypt ' + textencrypt);
+    searchLogo.style.display = 'none';
     pResult.innerText = textencrypt;
 }
+
+// btnYellow.addEventListener('click', colorYellow);
+
+function colorYellow() {
+    btnYellow.style.backgroundColor = '#FFFF00';
+}
+
+/* <button onClick="Mycolor()">Change Color</button>
+            <div id="myID">Hello World</div>
+            <script type="text/javascript">
+                function Mycolor() {
+                    var element = document.getElementById('myID');
+                    element.style.backgroundColor = '#900';
+                }
+            </script> */
