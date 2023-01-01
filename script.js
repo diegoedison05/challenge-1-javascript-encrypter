ca11970a79db785c53e41419e86878ad6bc233bd
DiegoEdison <diegoedison05@gmail.com>
2023-01-01 12:15:48 -0500

    Se creó la función de desencriptar

--- script.js
+++ script.js
@@ -64,33 +64,48 @@ function colorYellow() {
                 }
             </script> */
 
+btnDecrypt.addEventListener('click', decryptFunc);
+
 function decryptFunc() {
     //Desencriptar
     const textIn = textAreaIn.value;
     let textLower = textIn.toLowerCase();
-    // textLower = textLower.split('');
+    let i = 0;
     let textencrypt = '';
-    for (let i = 0; i < textLower.length; i++) {
-        if (textLower[i] == 'a' || textLower[i] == 'á') {
-            textencrypt = textencrypt + 'ai';
-        } else if (textLower[i] == 'e' || textLower[i] == 'é') {
-            textencrypt = textencrypt + 'enter';
-        } else if (textLower[i] == 'i' || textLower[i] == 'í') {
-            textencrypt = textencrypt + 'imes';
-        } else if (textLower[i] == 'o' || textLower[i] == 'ó') {
-            textencrypt = textencrypt + 'ober';
-        } else if (textLower[i] == 'u' || textLower[i] == 'ú') {
-            textencrypt = textencrypt + 'ufat';
+
+    while (i < textLower.length) {
+        // for (let i = 0; i < textLower.length; i++) {
+        if (textLower[i] == 'a') {
+            textencrypt = textencrypt + 'a';
+            i += 2;
+        } else if (textLower[i] == 'e') {
+            textencrypt = textencrypt + 'e';
+            i += 5;
+        } else if (textLower[i] == 'i') {
+            textencrypt = textencrypt + 'i';
+            i += 4;
+        } else if (textLower[i] == 'o') {
+            textencrypt = textencrypt + 'o';
+            i += 4;
+        } else if (textLower[i] == 'u') {
+            textencrypt = textencrypt + 'u';
+            i += 4;
+            console.log('o: ', i);
         } else {
             textencrypt = textencrypt + textLower[i];
+            i += 1;
         }
-    }
-    // console.log('textencrypt ' + textencrypt);
-    searchLogo.style.display = 'none';
-    pResult.innerText = textencrypt;
-}
 
-btnDecrypt.addEventListener('click', decryptFunc);
+        // if (i > textLower.length) {
+        //     console.log(i);
+        //     break;
+        // }
+    }
+    searchLogo.style.display = 'none';
+    pResult.style.display = 'none';
+    textOut.removeAttribute('hidden');
+    textOut.innerText = textencrypt;
+}
 
 btnCopy.addEventListener('click', copyText);
 
